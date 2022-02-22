f489e2c3c23ebcbc4459858dbd1fd09cf0508ef1
Ganesh Tiwari <ganesht049@gmail.com>
2022-02-22 11:10:29 +0530

    create qr code and put it on canvas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createRef, useRef } from "react";
+import QRcode from "qrcode";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const qrDivRef = createRef(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => {
+        QRcode.toCanvas(qrDivRef.current, "some text that we want to make qr off")
+      }}>Generate QR code</button>
+      <canvas ref={qrDivRef} id="qr-div"></canvas>
     </div>
   );
 }
